96a2eefda0b3c1721ce17390c0675a2535f5e3f4
aQua <aQuaYi@users.noreply.github.com>
2017-04-10 14:19:36 +0800

    提供了习题1.1.2的答案

--- 1.Fundamentals/1.1.BasicProgrammingModel/1.1.1.GiveTheValue/main.go
+++ 1.Fundamentals/1.1.BasicProgrammingModel/1.1.1.GiveTheValue/main.go
@@ -7,6 +7,9 @@ import (
 func main() {
 	// 因为以下计算过程是int类型的，所有每一步都会舍弃小数点部分。
 	fmt.Println((0 + 15) / 2)
+	// 这一题应该是考查极大的数的末尾精度问题，但是Golang没有出错，得出了正确答案。
 	fmt.Println(2.0e-6 * 100000000.1)
+	// 在Golang中 &&　与　||　是同级的。所以，遵循从左往右的循序。
 	fmt.Println(true && false || true && true)
+	fmt.Println(false && true || true) // 答案是true
 }
--- 1.Fundamentals/1.1.BasicProgrammingModel/README.md
+++ 1.Fundamentals/1.1.BasicProgrammingModel/README.md
@@ -2,3 +2,4 @@
 
 ## 练习
 1. [1.1.1](./1.1.1.GiveTheValue/main.go)
+2. [1.1.2](./1.1.2.GiveTheTypeAndValue/main.go)
