f8d9a1789d9013d4f5f8c045ab57b3de77d8eb16
aQua <aQuaYi@users.noreply.github.com>
2017-04-11 21:14:36 +0800

    提供了不带锁的版本。

--- 1.Fundamentals/1.1.BasicProgrammingModel/1.1.27.Binomal/main.go
+++ 1.Fundamentals/1.1.BasicProgrammingModel/1.1.27.Binomal/main.go
@@ -2,31 +2,24 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 var b map[string]float64
-var mutex sync.Mutex
-var count int
 
 func main() {
 	b = make(map[string]float64)
 	beginTime := time.Now()
 	fmt.Println(binomal(100, 50, 0.5))
 	fmt.Println("耗时", time.Since(beginTime))
-	fmt.Println(count, len(b))
 }
 
 func binomal(n, k int, p float64) float64 {
-	if n < k {
-		return 0
-	}
 	if n == 0 && k == 0 {
 		return 1
 	}
 
-	if n < 0 || k < 0 {
+	if n < 0 || k < 0 || n < k {
 		return 0
 	}
 
@@ -34,14 +27,9 @@ func binomal(n, k int, p float64) float64 {
 	if v, ok := b[key]; ok {
 		return v
 	}
-	v := (1-p)*binomal(n-1, k, p) + p*binomal(n-1, k-1, p)
 
-	{
-		mutex.Lock()
-		b[key] = v
-		count++
-		mutex.Unlock()
-	}
+	v := (1-p)*binomal(n-1, k, p) + p*binomal(n-1, k-1, p)
+	b[key] = v
 	return v
 
 }
