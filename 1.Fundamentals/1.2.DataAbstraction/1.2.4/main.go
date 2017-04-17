dc724df57372fe6628f409a5cf80b6875c9c18b7
aQua <aQuaYi@users.noreply.github.com>
2017-04-17 11:16:41 +0800

    添加了1.2.4的答案。

--- 1.Fundamentals/1.2.DataAbstraction/1.2.4/main.go
+++ 1.Fundamentals/1.2.DataAbstraction/1.2.4/main.go
@@ -1,8 +1,14 @@
 package main
+
 import (
 	"fmt"
 )
+
 func main() {
-fmt.Println("程序开始")
-	}
-	
+	fmt.Println("程序开始")
+	str1 := "hello"
+	str2 := str1
+	str1 = "world"
+	fmt.Println(str1)
+	fmt.Println(str2)
+}
