aed23f718a0e5d2987ecc19cc112b99842bc66b1
aQua <aQuaYi@users.noreply.github.com>
2017-04-17 17:11:14 +0800

    添加了1.2.7和1.2.8的答案

--- 1.Fundamentals/1.2.DataAbstraction/1.2.7/main.go
+++ 1.Fundamentals/1.2.DataAbstraction/1.2.7/main.go
@@ -1,8 +1,22 @@
 package main
+
 import (
 	"fmt"
 )
+
 func main() {
-fmt.Println("程序开始")
+	fmt.Println("程序开始")
+	s := "ABCdef"
+	fmt.Println(s)
+	fmt.Println(mystery(s))
+}
+
+func mystery(s string) string {
+	n := len(s)
+	if n <= 1 {
+		return s
 	}
-	
+	a := s[0 : n/2]
+	b := s[n/2 : n]
+	return b + a
+}
--- 1.Fundamentals/1.2.DataAbstraction/1.2.8/main.go
+++
@@ -1,8 +0,0 @@
-package main
-import (
-	"fmt"
-)
-func main() {
-fmt.Println("程序开始")
-	}
-	
