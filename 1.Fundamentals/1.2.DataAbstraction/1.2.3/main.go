d0d2740c5d65587d09823e556b02cdbf1985b5bd
aQua <aQuaYi@users.noreply.github.com>
2017-04-18 11:19:39 +0800

    看书不认真，1.2.1～1.2.3其实是可以需要编写一个库的，我都快写完了，才想起来。

--- 1.Fundamentals/1.2.DataAbstraction/1.2.3/main.go
+++ 1.Fundamentals/1.2.DataAbstraction/1.2.3/main.go
@@ -1,8 +1,29 @@
 package main
+
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
+
 func main() {
-fmt.Println("程序开始")
+	if len(os.Args) != 4 {
+		fmt.Println("请输入3个参数，正整数N和属于[0,1]的float64数min和max，要求min<max。")
 	}
-	
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("无法获取正整数N", err)
+		os.Exit(1)
+	}
+	min, err := strconv.ParseFloat(os.Args[2], 64)
+	if err != nil {
+		fmt.Println("无法获取下限min", err)
+		os.Exit(1)
+	}
+	max, err := strconv.ParseFloat(os.Args[3], 64)
+	if err != nil || min >= max {
+		fmt.Println("无法获取上限max，或者min>=max。", err)
+		os.Exit(1)
+	}
+
+}
