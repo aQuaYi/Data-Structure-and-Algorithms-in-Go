072933bfdcfa0de2083bc3e80d8e71e3c2b49be1
aQua <aQuaYi@users.noreply.github.com>
2017-04-22 21:04:38 +0800

    添加了1.3.15的答案

--- 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.15/main.go
+++ 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.15/main.go
@@ -1,13 +1,30 @@
+//我输出的是int,原理是一样的。
 package main
 
 import (
+	"Algorithms-in-Golang/queue"
 	"fmt"
+	"math/rand"
 )
 
 func main() {
 	fmt.Println("程序开始")
 
+	k := 10
+	q := queue.New()
+	r := rand.Intn(1000)
+	for i := 0; i < k+r; i++ {
+		q.Enqueue(i)
+	}
+	fmt.Printf("%d=", r)
 
-
+	n := q.Size()
+	c := q.Iterator()
+	for c.HasNext() {
+		result := c.Next().(int)
+		if n == k {
+			fmt.Printf("%v\n", result)
+		}
+		n--
+	}
 }
-	
