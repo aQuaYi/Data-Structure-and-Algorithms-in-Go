be5666b84c4aadfacdc8fffd734878c1e49d4997
aQua <aQuaYi@users.noreply.github.com>
2017-04-22 19:41:55 +0800

    添加了1.3.6的答案

--- 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.6/main.go
+++ 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.6/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"Algorithms-in-Golang/queue"
+	"Algorithms-in-Golang/stack"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("程序开始")
+	q := queue.New()
+	for i := 0; i < 10; i++ {
+		q.Enqueue(i)
+	}
+	c := q.Iterator()
+	for c.HasNext() {
+		fmt.Printf("%v ", c.Next())
+	}
 
+	fmt.Println()
 
+	s := stack.New()
+	for !q.IsEmpty() {
+		s.Push(q.Dequeue())
+	}
+	for !s.IsEmpty() {
+		q.Enqueue(s.Pop())
+	}
 
+	c = q.Iterator()
+	for c.HasNext() {
+		fmt.Printf("%v ", c.Next())
+	}
 }
-	
