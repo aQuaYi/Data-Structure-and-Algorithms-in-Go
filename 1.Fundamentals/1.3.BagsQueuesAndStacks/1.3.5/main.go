3ac6b51f6abd2b8d23185dfebc37d24ef9ec720f
aQua <aQuaYi@users.noreply.github.com>
2017-04-22 19:33:36 +0800

    添加了1.3.5的答案

--- 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.5/main.go
+++ 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.5/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"Algorithms-in-Golang/stack"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("程序开始")
-
-
-
+	n := 50
+	fmt.Printf("%d=", n)
+	stk := stack.New()
+	for n > 0 {
+		stk.Push(n % 2)
+		n = n / 2
+	}
+	c := stk.Iterator()
+	for c.HasNext() {
+		fmt.Print(c.Next().(int))
+	}
+	fmt.Println()
 }
-	
