9bb68395a59494995a723ec030d4c4e1185a515d
aQua <aQuaYi@users.noreply.github.com>
2017-04-22 19:27:52 +0800

    添加了1.3.4的答案

--- 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.4/main.go
+++ 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.4/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"Algorithms-in-Golang/stack"
 	"fmt"
+	"strings"
+)
+
+var (
+	p = map[string]string{
+		")": "(",
+		"]": "[",
+		"}": "{",
+	}
 )
 
 func main() {
 	fmt.Println("程序开始")
-
-
-
+	check("[(])")
+	check("[()]{}{[()()]()}")
 }
-	
+
+func check(s string) {
+	stk := stack.New()
+	ss := strings.Split(s, "")
+
+	for _, v := range ss {
+		switch v {
+		case "(", "[", "{":
+			stk.Push(v)
+		case ")", "]", "}":
+			if stk.Pop().(string) != p[v] {
+				fmt.Println("false")
+				return
+			}
+		default:
+			fmt.Printf("%s当中有其他字符\n", s)
+		}
+	}
+	fmt.Println("true")
+}
