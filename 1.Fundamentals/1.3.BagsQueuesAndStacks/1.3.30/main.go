341868308c707ccdb6d5b106f8e4f26a7d4ab87b
aQua <aQuaYi@users.noreply.github.com>
2017-04-23 13:53:28 +0800

    开始了1.3.30的答案，未完成。

--- 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.30/main.go
+++ 1.Fundamentals/1.3.BagsQueuesAndStacks/1.3.30/main.go
@@ -1,13 +1,73 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("程序开始")
+	fmt.Println("reverse长度为0的链")
+	fmt.Println(reverse(nil))
 
+	fmt.Println("reverse长度为１的链")
+	first := newNode(0)
+	fmt.Println(first.item)
+	fmt.Println(reverse(first).item)
+	n := 10
+	fmt.Printf("reverse长度为%d的链\n", n)
 
+	last := first
+	for i := 1; i < n; i++ {
+		newLast := newNode(i)
+		last.next = newLast
+		last = newLast
+	}
+
+	temp := first
+	for temp != nil {
+		fmt.Printf("%v ", temp.item)
+		temp = temp.next
+	}
+	fmt.Println()
+
+	first = reverse(first)
+	temp = first
+	for temp != nil {
+		fmt.Printf("%v ", temp.item)
+		temp = temp.next
+	}
+	fmt.Println()
+}
+
+func reverse(first *node) *node {
+	if first == nil {
+		return nil
+	}
+	if first.next == nil {
+		return first
+	}
+	if first.next.next == nil {
+		result := first.next
+		first.next.next = first
+		first.next = nil
+		return result
+	}
+
+	fn := first.next
+	first.next = nil
+	return reverse(fn).nextTo(first)
 
 }
-	
+
+type node struct {
+	item interface{}
+	next *node
+}
+
+func newNode(item interface{}) *node {
+	return &node{
+		item: item,
+	}
+}
+
+func (n *node) nextTo(anther *node) *node {
+	n.next = anther
+	return n
+}
