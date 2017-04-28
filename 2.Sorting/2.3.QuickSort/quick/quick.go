0918645a806013fdde37f63426c42da902986f40
aQua <aQuaYi@users.noreply.github.com>
2017-04-28 21:31:23 +0800

    修改了快排算法的实现，使之可以处理重复元素的的情况。

--- 2.Sorting/2.3.QuickSort/quick/quick.go
+++ 2.Sorting/2.3.QuickSort/quick/quick.go
@@ -28,14 +28,14 @@ func Sort(a Interface) {
 func partition(a sort.Interface) int {
 	i, j := 1, a.Len()-1
 	for {
-		for a.Less(i, 0) && i < a.Len()-1 {
+		for !a.Less(0, i) && i < a.Len()-1 {
 			// i<a.Len()-1 保证了当i不会为a.Len()
 			// 例如，当a[0]为最大值时，i会一直变大。直到i++后，i==a.Len()
 			// 那么在下一个a.Less(i,0)时，会产生"index out of range"错误。
 			// 当a.Len()==2时，这种情况很容易发生。
 			i++
 		}
-		for a.Less(0, j) && j > 0 {
+		for !a.Less(j, 0) && j > 0 {
 			// j>0, 保证了j不会为-1
 			// 例如，当a[0]为最小值时，j会一直变小。直到j--后，j==-1
 			// 那么在下一个a.Less(0,j)时，会产生"index out of range"错误。
--- 2.Sorting/2.3.QuickSort/quick/quick_test.go
+++ 2.Sorting/2.3.QuickSort/quick/quick_test.go
@@ -1,6 +1,7 @@
 package quick
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -32,16 +33,30 @@ func (q quick) IsSorted() bool {
 	}
 	return true
 }
-
+func Test_Sort_with_Repeating(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	n := 10
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	fmt.Println(q)
+	Sort(q)
+	fmt.Println(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
 func Test_Sort(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	q := make(quick, 10)
 	for i := 0; i < len(q); i++ {
 		q[i] = len(q) - i - 1
 	}
-	t.Log(q)
+	fmt.Println(q)
 	Sort(q)
-	t.Log(q)
+	fmt.Println(q)
 	if !q.IsSorted() {
 		t.Error("没能排序好")
 	}
