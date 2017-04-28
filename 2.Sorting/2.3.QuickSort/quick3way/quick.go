a0fa48200e0e2f463415f75782fb01423b218d6b
aQua <aQuaYi@users.noreply.github.com>
2017-04-28 14:59:31 +0800

    添加了3向切分快排，可以处理包含重复元素的序列了。

---
+++ 2.Sorting/2.3.QuickSort/quick3way/quick.go
@@ -0,0 +1,41 @@
+package quick3way
+
+import "math/rand"
+import "sort"
+
+//Interface 是quick排序的接口
+type Interface interface {
+	sort.Interface
+	More(int, int) bool
+	Divide(int, int) Interface
+}
+
+//Sort 使用quickSort算法进行排序。
+func Sort(a Interface) {
+	if a.Len() <= 1 {
+		return
+	}
+	j := rand.Intn(a.Len())
+	a.Swap(0, j)
+	lt, gt := partition(a)
+	Sort(a.Divide(0, lt))
+	Sort(a.Divide(gt+1, a.Len()))
+}
+
+func partition(a Interface) (int, int) {
+	lt, i, gt := 0, 1, a.Len()-1
+	for i <= gt {
+		switch {
+		case a.Less(i, lt):
+			a.Swap(lt, i)
+			lt++
+			i++
+		case a.More(i, lt):
+			a.Swap(i, gt)
+			gt--
+		default:
+			i++
+		}
+	}
+	return lt, gt
+}
---
+++ 2.Sorting/2.3.QuickSort/quick3way/quick_test.go
@@ -0,0 +1,133 @@
+package quick3way
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+type quick []int
+
+func (q quick) Len() int {
+	return len(q)
+}
+
+func (q quick) Less(i, j int) bool {
+	return q[i] < q[j]
+}
+
+func (q quick) More(i, j int) bool {
+	return q[i] > q[j]
+}
+
+func (q quick) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+}
+
+func (q quick) Divide(i, j int) Interface {
+	return q[i:j]
+}
+
+func (q quick) IsSorted() bool {
+	for i := 1; i < q.Len(); i++ {
+		if q.More(i-1, i) {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_Sort(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	n := 10
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	fmt.Println(q)
+	Sort(q)
+	fmt.Println(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
+
+func Test_Sort_100(t *testing.T) {
+	n := 100
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	Sort(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
+
+func Test_Sort_1000(t *testing.T) {
+	n := 1000
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	Sort(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
+
+func Test_Sort_10000(t *testing.T) {
+	n := 10000
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	Sort(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
+
+func Test_Sort_100000(t *testing.T) {
+	n := 100000
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	Sort(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
+
+func Test_Sort_1000000(t *testing.T) {
+	n := 1000000
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	Sort(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
+
+func Test_Sort_10000000(t *testing.T) {
+	n := 10000000
+	q := make(quick, n*3)
+	for i := 0; i < n; i++ {
+		q[i] = n - i - 1
+		q[i+n], q[i+n+n] = q[i], q[i]
+	}
+	Sort(q)
+	if !q.IsSorted() {
+		t.Error("没能排序好")
+	}
+}
