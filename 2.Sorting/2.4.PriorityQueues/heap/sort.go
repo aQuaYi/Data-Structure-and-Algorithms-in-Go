03102ad0072db98d16e2848a2f0387d96d5a7ec1
aQua <aQuaYi@users.noreply.github.com>
2017-05-03 08:35:18 +0800

    修改了名称，让堆排序过程更清晰。

--- 2.Sorting/2.4.PriorityQueues/heap/sort.go
+++ 2.Sorting/2.4.PriorityQueues/heap/sort.go
@@ -10,27 +10,27 @@ type Interface interface {
 
 //Sort 使用堆排序
 func Sort(a Interface) {
-	reheapify(a)
+	heapify(a)
 	heapSort(a)
 }
 
-//reheapify 堆有序化
-//reheapify后，根节点是序列中的最大值。
+//heapify 堆有序化
+//heapify后，根节点是序列中的最大值。
 //a[i]的son节点是a[2*i+1]和a[2*i+2]
 //a[i]的father节点是a[(i-1)/2]
 //root节点是a[0]
-func reheapify(a Interface) {
+func heapify(a Interface) {
 	n := a.Len()
 	for i := n/2 - 1; i >= 0; i-- {
 		//a[n/2-1]是序号最大的有son节点的元素
 		//i:=n-1也可以堆有序化，但是，浪费了很多时间
-		maxTop(a, i)
+		reheapify(a, i)
 	}
 }
 
-//maxTop前，如果以i节点的两个子节点为root节点的两个子完全二叉树是堆有序的。
-//maxTop后，以i节点为root节点的子完全二叉树，堆有序。而且，i节点是这个子完全二叉树中的最大值。
-func maxTop(a Interface, i int) {
+//reheapify前，如果以i节点的两个子节点为root节点的两个子完全二叉树是堆有序的。
+//reheapify后，以i节点为root节点的子完全二叉树，堆有序。而且，i节点是这个子树中的最大值。
+func reheapify(a Interface, i int) {
 	n := a.Len()
 	for 2*i+1 < n { //当i节点存在son节点的时候，继续循环
 		iSon := 2*i + 1
@@ -53,6 +53,6 @@ func heapSort(a Interface) {
 		n--
 		a.Swap(0, n)       //把序列a中的最大值，放在序列尾部
 		a = a.Divide(0, n) //把已经已经归为的a的最大值与其余部分隔离开
-		maxTop(a, 0)       //让a重新堆有序化
+		reheapify(a, 0)    //让a重新堆有序化
 	}
 }
